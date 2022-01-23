3e91f074c555d91c07cce49f497373d672475d8c
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-01-24 03:26:01 +0900

    Chore: 言語・タイムゾーンを日本に設定 #6

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,10 @@ module GrowlVoiceChanger
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
 
     # Don't generate system test files.
     config.generators.system_tests = nil
