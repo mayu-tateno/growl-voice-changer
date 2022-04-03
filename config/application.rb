4878bd9fa0c83822dc911fa60a3e02ed75271288
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-04-03 11:26:24 +0900

    Refactor: rubocopの指摘事項を手動で修正#11

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module GrowlVoiceChanger
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :ja
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
