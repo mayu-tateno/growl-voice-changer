3b2ba1894dc5fd7e477bdd8b58cd174a1a428aff
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-04-07 16:24:45 +0900

    Feat: rails g sorcery:installを実行・usersテーブルを作成#12

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_07_050656) do
+ActiveRecord::Schema.define(version: 2022_04_07_053747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.string "name", null: false
+    t.string "role", default: "0", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
 
 end
