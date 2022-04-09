03ac7d7e27729436aed631ec4f1d38ed722575fe
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-04-09 18:17:52 +0900

    Feat: ユーザー作成時自動でログインするよう設定#12

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      # auto_login(@user)
+      auto_login(@user)
       redirect_to root_url
     else
       render :new
