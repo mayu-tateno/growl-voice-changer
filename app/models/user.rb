daa2b267afb2c3c79f8c7517bc1f481a3cee0ef3
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-04-09 15:39:17 +0900

    Feat: パスワードの最低文字数を6文字以上に設定#12

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
