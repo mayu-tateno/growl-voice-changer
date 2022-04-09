85ec10e612a95c2c684822fa034eea287181c51b
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-04-09 16:54:34 +0900

    Style: rubocopの指摘事項を手動で修正#12

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -134,7 +134,12 @@ Rails.application.config.sorcery.configure do |config|
   # config.instagram.secret = ""
   # config.instagram.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=instagram"
   # config.instagram.user_info_mapping = {:email => "username"}
-  # config.instagram.access_permissions = ["basic", "public_content", "follower_list", "comments", "relationships", "likes"]
+  # config.instagram.access_permissions = ["basic",
+  #                                        "public_content",
+  #                                        "follower_list",
+  #                                        "comments",
+  #                                        "relationships",
+  #                                        "likes"]
   #
   # config.github.key = ""
   # config.github.secret = ""
