33b7f27800c19bedbb724620dec2841e5612999d
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-06-18 00:23:11 +0900

    Feat: descriptionに最大30字の制限を設定 #23

--- app/models/voice.rb
+++ app/models/voice.rb
@@ -1,7 +1,7 @@
 class Voice < ApplicationRecord
   belongs_to :user
 
-  validates :description, presence: true
+  validates :description, presence: true, length: { maximum: 30 }
 
   enum status: { open: 0, closed: 1 }
 end
