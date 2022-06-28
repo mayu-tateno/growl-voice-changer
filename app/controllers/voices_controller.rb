fd139b273fa4ac168d391e94d176ebe5726cf58e
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-06-28 20:33:54 +0900

    Feat: 非ログインユーザーの場合、ゲストユーザーとして音声を保存する機能追加 #39

--- app/controllers/voices_controller.rb
+++ app/controllers/voices_controller.rb
@@ -2,7 +2,19 @@ class VoicesController < ApplicationController
   def new; end
 
   def create
-    voice = current_user.voices.build(voice_params) if logged_in?
+    if logged_in?
+      voice = current_user.voices.build(voice_params)
+    else
+      random_value = SecureRandom.hex
+      guest_user = User.create!(
+        name: "ゲストユーザー",
+        email: "guest_#{random_value}@example.com",
+        password: random_value,
+        password_confirmation: random_value,
+        role: :guest
+      )
+      voice = guest_user.voices.build(voice_params)
+    end
 
     if voice.save
       render json: { url: root_url }
