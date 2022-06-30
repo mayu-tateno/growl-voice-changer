cf795498416ae1fca83230b36521a236191f9d42
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-06-30 22:48:13 +0900

    Feat: destroyアクションとその処理、ルーティング、削除ボタンへリンクを追加 #40

--- app/controllers/voices_controller.rb
+++ app/controllers/voices_controller.rb
@@ -27,6 +27,12 @@ class VoicesController < ApplicationController
     end
   end
 
+  def destroy
+    voice = current_user.voices.find(params[:id])
+    voice.destroy!
+    redirect_to root_url, dark: '音声を削除しました'
+  end
+
   private
 
   def set_voice
