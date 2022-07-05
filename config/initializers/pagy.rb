b0df3846e84de97072e4468e61590353282a6132
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-07-05 11:39:54 +0900

    Feat: pagyにBootstrapを適用 #52

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -122,7 +122,7 @@ Pagy::DEFAULT[:items]  = 10
 
 # Bootstrap extra: Add nav, nav_js and combo_nav_js helpers and templates for Bootstrap pagination
 # See https://ddnexus.github.io/pagy/extras/bootstrap
-# require 'pagy/extras/bootstrap'
+require 'pagy/extras/bootstrap'
 
 # Bulma extra: Add nav, nav_js and combo_nav_js helpers and templates for Bulma pagination
 # See https://ddnexus.github.io/pagy/extras/bulma
