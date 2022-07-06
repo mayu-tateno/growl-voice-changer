b071d69f4b89466f90a6ea23a1896dd4641b366b
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-07-06 22:29:42 +0900

    Feat: お題作成に必要なコントローラーを記述 #67

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,22 @@
 class TopicsController < ApplicationController
   def new
+    @topic = current_user.topics.build
+  end
+
+  def create
+    topic = current_user.topics.build(topic_params)
+
+    if topic.save
+      redirect_to root_path, dark: 'お題を作成しました'
+    else
+      flash.now[:danger] = 'お題作成に失敗しました'
+      render :new
+    end
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:body)
   end
 end
