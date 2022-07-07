5a8850f13e5214bc876047e9d50c701ee1cea8cf
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-07-07 17:08:33 +0900

    Fix: Voiceモデルに設定し忘れていたgrowl_voiceのバリデーションを設定 #70

--- app/models/voice.rb
+++ app/models/voice.rb
@@ -2,6 +2,7 @@ class Voice < ApplicationRecord
   belongs_to :user
   mount_uploader :growl_voice, GrowlVoiceUploader
 
+  validates :growl_voice, presence: true
   validates :description, presence: true, length: { maximum: 30 }
 
   enum status: { open: 0, closed: 1 }
