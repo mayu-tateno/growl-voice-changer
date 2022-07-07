7c0096c82086851770eaa72be08e2a2b7fa3434d
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-07-07 17:23:35 +0900

    Feat: ルーティングを追加 #70

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   resources :users, only: %i[new create]
   resources :voices
   resources :topics, only: %i[new create index show]
+  resources :answers, only: %i[new create]
 end
