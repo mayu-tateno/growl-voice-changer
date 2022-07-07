0a33fbad6e3d1fb2b18f862a6eeb0f6e9856c872
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-07-07 17:10:42 +0900

    Feat: growl_voiceカラムにアップローダーをマウント #70

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,7 @@
 class Answer < ApplicationRecord
   belongs_to :user
   belongs_to :topic
+  mount_uploader :growl_voice, GrowlVoiceUploader
 
   validates :growl_voice, presence: true
   validates :description, presence: true, length: { maximum: 30 }
