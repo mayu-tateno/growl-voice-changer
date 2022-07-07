f790284e27f9aa98db3e9902f0ccbd13817c3804
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-07-07 17:07:24 +0900

    Feat: Answerモデルに各カラムのバリデーションを追加 #70

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,7 @@
 class Answer < ApplicationRecord
   belongs_to :user
   belongs_to :topic
+
+  validates :growl_voice, presence: true
+  validates :description, presence: true, length: { maximum: 30 }
 end
