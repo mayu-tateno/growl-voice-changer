70541556b5910ce16401383b2ccfeb576a6734c3
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-07-10 19:57:10 +0900

    Feat: users#destroyのルーティングを設定 #83

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
 
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create destroy]
   resources :voices
   resources :topics, only: %i[new create index show] do
     resources :answers, only: %i[new create show edit update destroy]
