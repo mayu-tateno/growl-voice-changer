3463ef507dbc0e605fe6d9599256a08fde42f645
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-07-11 11:54:32 +0900

    Feat: マイページ編集画面でユーザー情報を編集する機能を実装 #84

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,8 +1,29 @@
 class MypagesController < ApplicationController
-  before_action :require_login, only: %i[show]
+  before_action :require_login, only: %i[show edit create]
 
   def show
     @pagy_voices, @voices = pagy(current_user.voices.order(created_at: :desc), page_param: :page_voices)
     @pagy_answers, @answers = pagy(current_user.answers.includes(:topic).order(created_at: :desc), page_param: :page_answers)
+  end
+
+  def edit
+    @user = User.find(current_user.id)
+  end
+
+  def update
+    @user = User.find(current_user.id)
+
+    if @user.update(user_params)
+      redirect_to mypage_path, dark: 'ユーザー情報を更新しました'
+    else
+      flash.now[:danger] = 'ユーザー情報の更新に失敗しました'
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
