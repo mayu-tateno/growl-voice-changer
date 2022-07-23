f96f8a02c181c38fe8694d63d2a2626860755249
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-07-23 23:58:44 +0900

    Feat: Authenticationとの関連付けを追加 #51

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :voices, dependent: :destroy
   has_many :topics, dependent: :destroy
   has_many :answers, dependent: :destroy
+  has_many :authentications, dependent: :destroy
+  accepts_nested_attributes_for :authentications
 
   validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
