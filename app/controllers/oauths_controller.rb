1112f87d52089d7b0a67f338abe67c1b8b902f81
mayu-tateno <87419889+mayu-tateno@users.noreply.github.com>
2022-07-24 23:29:25 +0900

    Feat: oauthsコントローラーを追加 #51

---
+++ app/controllers/oauths_controller.rb
@@ -0,0 +1,29 @@
+class OauthsController < ApplicationController
+  def oauth
+    login_at(params[:provider])
+  end
+
+  def callback
+    provider = auth_params[:provider]
+    if (@user = login_from(provider))
+      redirect_to root_path, dark: "#{provider.titleize}でログインしました"
+    else
+      begin
+        binding.pry
+        @user = create_from(provider)
+        binding.pry
+        reset_session
+        auto_login(@user)
+        redirect_to root_path, dark: "#{provider.titleize}でログインしました"
+      rescue StandardError
+        redirect_to root_path, danger: "#{provider.titleize}でのログインに失敗しました"
+      end
+    end
+  end
+
+  private
+
+  def auth_params
+    params.permit(:code, :provider)
+  end
+end
